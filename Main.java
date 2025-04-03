9add6a9e9624295862efce8ae4c5ec5a06dc9e18
vikasrajojha1 <vikasrajojha1@gmail.com>
2025-04-03 19:16:41 +0530

    Java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    int a = 0;  
+    int b = 1, n=5;
+    
+    System.out.print(a+ " " + b);
+    int c=0;
+    for(int i=2; i <n ; i++) {
+      c = a + b;
+      System.out.print(" " + c);
+      a = b;
+      b = c;
+    }
+  }
+}
